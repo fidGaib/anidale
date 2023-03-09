38b5ff9cdcc2c3ccf77a6d326d93aabd2aa7bc27
fidGaib <libenkemper@gmail.com>
2023-03-10 01:49:53 +0500

    init

---
+++ server/src/main.ts
@@ -0,0 +1,4 @@
+import { killerFicha } from './modules/k.js';
+const Message = 'hello wrold';
+console.log(Message);
+killerFicha();
---
+++ server/src/modules/k.ts
@@ -0,0 +1,3 @@
+export const killerFicha = (): void => {
+    return console.log('import work!')
+}
