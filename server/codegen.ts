a8f82c2505b6e79497ce5eeae40acec481079874
mukkumayc <sk9078@ya.ru>
2023-03-16 04:30:04 +0600

    server: fix codegen.ts schema and generate paths

--- server/codegen.ts
+++ server/codegen.ts
@@ -1,9 +1,9 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-  schema: 'schema/**/*.graphql',
+  schema: 'src/schema/**/*.graphql',
   generates: {
-    './schema/resolvers-types.ts': {
+    './src/schema/resolvers-types.ts': {
       plugins: ['typescript', 'typescript-resolvers'],
       config: {
         scalars: {
