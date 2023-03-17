77efce4e6648ef1ae95e4c690890221d84bbb5e7
fidGaib <libenkemper@gmail.com>
2023-03-17 22:18:09 +0500

    fix user schema

--- server/src/api/dtos/user-dto.ts
+++ server/src/api/dtos/user-dto.ts
@@ -2,17 +2,7 @@ import { User } from '@prisma/client'
 
 export default class UserDto {
   id: number
-  email: string
-  login: string
-  isActivated: boolean
-  avatar: string
-  createdAt: Date
   constructor(model: User) {
-    this.email = model.email
     this.id = model.id
-    this.isActivated = model.isActivated
-    this.login = model.login
-    this.avatar = model.avatar
-    this.createdAt = model.createdAt
   }
 }
