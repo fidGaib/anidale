5475c14865730ce14db53076b5d8556d357fdd96
mukkumayc <sk9078@ya.ru>
2023-03-19 03:32:28 +0600

    server: change hot reload code

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,25 +10,30 @@ handlers.forEach((h: any) => {
   else app.use(h)
 })
 
-async function stopApplication(server: Server) {
+function stopApplication(server: Server) {
   server.close()
   return new Promise((resolve) => {
-    server.on('close', resolve)
+    server.on('close', () => {
+      resolve(0)
+    })
   })
 }
 
 async function onHotReload(server: Server) {
-  if (import.meta.hot) await import.meta.hot.data.stopping
-  if (import.meta.hot) {
-    import.meta.hot.on('vite:beforeFullReload', () => {
-      console.log('Performing an HMR reload...')
-      if (import.meta.hot) import.meta.hot.data.stopping = stopApplication(server)
+  const hotContext = import.meta.hot
+  if (hotContext) {
+    await hotContext.data.stopping
+
+    hotContext.on('vite:beforeFullReload', async () => {
+      console.log('\nPerforming an HMR reload...')
+      hotContext.data.stopping = stopApplication(server)
     })
   }
 }
 
-const start = () => {
+const start = async () => {
   try {
+    if (import.meta.hot) await import.meta.hot.data.stopping
     const server = app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`))
     onHotReload(server)
   } catch (e) {
