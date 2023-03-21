827ee10504798f935a981bf2cf56eb7ae490cb0c
mukkumayc <sk9078@ya.ru>
2023-03-21 06:19:17 +0600

    client: rewrite MakePost logic

--- client/src/features/profile/ui.tsx
+++ client/src/features/profile/ui.tsx
@@ -1,12 +1,11 @@
 import { useMutation } from '@apollo/client'
-import { useEffect, useRef } from 'react'
+import { ChangeEventHandler, KeyboardEventHandler } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { useViewer } from '@/entities/viewer'
 import { CREATE_POST } from '@/shared/graphql/schema'
-import { useAutosizeTextArea } from '@/shared/hooks/useAutosizeTextArea'
 import Icon from '@/shared/icons'
 
-import { usePostStore } from './module'
 import cl from './ui.module.less'
 
 interface Props {
@@ -15,82 +14,85 @@ interface Props {
     avatar: string
   } | null
 }
+
+interface InputValues {
+  description: string
+  images: File[]
+}
+
+const resizeTextarea: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+  const el = e.target
+  if (el) {
+    el.style.height = '0px'
+    el.style.height = el.scrollHeight + 'px'
+  }
+}
+
 export const MakePost = ({ user }: Props) => {
-  const store = usePostStore((state) => state)
-  const textRef = useRef(null)
-  const blockRef = useRef(null)
-  useAutosizeTextArea(textRef.current, store.description)
+  const { register, handleSubmit, watch, setValue } = useForm<InputValues>()
+  const images = watch('images')
+
   const [createPost, {}] = useMutation(CREATE_POST)
   const myUser = useViewer()
-  const send = () => {
-    const formData = new FormData()
-    formData.append('description', store.description)
-    store.images.forEach((file) => formData.append('images', file))
-    createPost({
-      variables: { owner: myUser?.id, description: formData.get('description'), images: formData.get('images') },
-    }).then((res) => {
-      const post = res.data.createPost
-      store.addPost([post])
-      store.changeText('')
-      store.images = []
-    })
-  }
-  const pressEnter = ({ shiftKey, key }: KeyboardEvent) => {
-    if (key === 'Enter' && !shiftKey) send()
-  }
-  useEffect(() => {
-    if (!store.description.trim()) return
-    document.addEventListener('keydown', pressEnter, false)
-    return () => {
-      document.removeEventListener('keydown', pressEnter, false)
+
+  const onSubmit: SubmitHandler<InputValues> = ({ images, description }) => {
+    if (!description && images.length === 0) {
+      alert('Не могу отправить пустой пост')
+      return
     }
-  }, [store.description])
+
+    createPost({ variables: { owner: myUser?.id, description, images } })
+  }
+
+  const handleRemoveImage = (image: File) =>
+    setValue(
+      'images',
+      Array.from(images).filter((el) => el !== image),
+    )
+
+  const handleKeydown: KeyboardEventHandler<HTMLTextAreaElement> = ({ key, shiftKey }) => {
+    if (key === 'Enter' && !shiftKey) handleSubmit(onSubmit)()
+  }
+
   return (
     <>
-      <p className={cl.error}>{store.error}</p>
-      <div ref={blockRef} className={cl.wrapper}>
+      <div className={cl.wrapper}>
         <div className={cl.wrapperColumn}>
-          <img src={user?.avatar} className={cl.avatar} />
-          <textarea
-            ref={textRef}
-            onChange={(e) => store.changeText(e.target.value)}
-            value={store.description}
-            placeholder='Что у вас нового?'
-          ></textarea>
-          <label htmlFor='file' className={cl.label}>
-            <Icon id='add_photo' className={cl.addPhoto} />
-          </label>
-          <input
-            multiple
-            type='file'
-            hidden
-            id='file'
-            accept='image/*'
-            onChange={(e) => {
-              store.setFiles(e.target.files || [], store)
-            }}
-          />
-          <Icon id='send' onClick={send} />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <img src={user?.avatar} className={cl.avatar} />
+            <textarea
+              {...register('description', { onChange: resizeTextarea })}
+              placeholder='Что у вас нового?'
+              onKeyDown={handleKeydown}
+            />
+            <label className={cl.label}>
+              <Icon id='add_photo' className={cl.addPhoto} />
+              <input {...register('images')} multiple type='file' hidden accept='image/*' />
+            </label>
+            <button type='submit'>
+              <Icon id='send' />
+            </button>
+          </form>
         </div>
-        {store.images.length ? (
-          <div className={cl.wrapperColumn2}>
-            {store.images.map((images) => (
-              <img
-                className={cl.preview}
-                key={images.size}
-                src={URL.createObjectURL(images)}
-                onClick={() => {
-                  let remove = store.images.filter((item) => item !== images)
-                  store.images = []
-                  store.setFiles(remove, store)
-                }}
-              />
-            ))}
-          </div>
-        ) : (
-          ''
-        )}
+        {images && images.length ? <ImagesPreview images={images} onRemove={handleRemoveImage} /> : ''}
       </div>
     </>
+  )
+}
+
+function ImagesPreview(props: { images: any[]; onRemove: (image: File) => void }) {
+  if (!props.images) return <></>
+
+  return (
+    <div className={cl.wrapperColumn2}>
+      {Array.from(props.images).map((image) => (
+        <img
+          className={cl.preview}
+          key={image.size}
+          src={URL.createObjectURL(image)}
+          onClick={() => props.onRemove(image)}
+        />
+      ))}
+    </div>
   )
 }
