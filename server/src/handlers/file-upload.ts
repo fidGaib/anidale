fac9b4077e4351bfbae7cc56f153b552b8778d6d
mukkumayc <sk9078@ya.ru>
2023-03-21 06:15:07 +0600

    server: remove express-fileupload

--- server/src/handlers/file-upload.ts
+++ server/src/handlers/file-upload.ts
@@ -1,8 +1,6 @@
 import express from 'express'
-import fileUpload from 'express-fileupload'
 import { fileURLToPath } from 'url'
 
 export const __dirname = fileURLToPath(new URL('../../', import.meta.url))
 export const ExpressStatic = { path: '/storage', func: express.static(__dirname + './storage/') }
 export const ExpressStatic2 = express.static(__dirname + './storage/')
-export const FileUpload = fileUpload({ createParentPath: true })
--- server/src/handlers/index.ts
+++ server/src/handlers/index.ts
@@ -1,7 +1,7 @@
 import { CookieParser } from './cookie-parser'
 import { cors } from './cors'
 import { expressJson } from './express-json'
-import { ExpressStatic, ExpressStatic2, FileUpload } from './file-upload'
+import { ExpressStatic, ExpressStatic2 } from './file-upload'
 import { Yoga } from './yoga'
 
-export default [cors, expressJson, ExpressStatic2, FileUpload, CookieParser, Yoga, ExpressStatic]
+export default [cors, expressJson, ExpressStatic2, CookieParser, Yoga, ExpressStatic]
