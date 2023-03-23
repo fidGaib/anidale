ae67bab699ae65173719986021c811f6bc369bdb
mukkumayc <sk9078@ya.ru>
2023-03-23 06:05:39 +0600

    client: change codegen documents to one file

--- client/codegen.ts
+++ client/codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
   schema: 'http://localhost:5000/graphql',
-  documents: ['src/**/*.tsx', 'src/**/*.ts'],
+  documents: ['src/shared/graphql/schema.ts'],
   // verbose: true,
   // debug: true,
   ignoreNoDocuments: true, // for better experience with the watcher
