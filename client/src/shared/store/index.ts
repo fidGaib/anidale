4185bc901d42eaa6aff3b3d386a8cad551992b46
mukkumayc <sk9078@ya.ru>
2023-03-27 20:29:15 +0600

    client: remove deprecated StateSelector

--- client/src/shared/store/index.ts
+++ client/src/shared/store/index.ts
@@ -1,9 +1,8 @@
-import { StateSelector } from 'zustand'
 import { shallow } from 'zustand/shallow'
 
 import { usePostStore as Post } from '@/features/profile/module'
 import { PostStore } from '@/features/profile/types'
 
-const usePostStore = <T>(cb: StateSelector<PostStore, T>) => Post(cb, shallow)
+const usePostStore = <T>(cb: (store: PostStore) => T) => Post(cb, shallow)
 
 export { usePostStore }
